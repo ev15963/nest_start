90a5bcf610bbb132bebed9381648e1d24b98761c
ev15963 <ev15963@hs.ac.kr>
2022-10-05 16:05:54 +0900

    dto req, res 구별, cats, apitest 폴더 분류

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ApitestModule } from './apitest.module';
+import { ApitestModule } from './modules/apitest/apitest.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats.module';
+import { CatsModule } from './modules/cats/cats.module';
 console.log('module in');
 @Module({
   imports: [
