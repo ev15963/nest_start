59aa5fb1ad208bc395c7907445f194255afba486
ev15963 <ev15963@hs.ac.kr>
2022-10-07 20:07:11 +0900

    매분 5초마다

--- src/modules/weatherapi/weatherapi.service.ts
+++ src/modules/weatherapi/weatherapi.service.ts
@@ -4,7 +4,7 @@ import { Cron } from "@nestjs/schedule";
 @Injectable()
 export class WeatherapiService {
     private readonly logger = new Logger(WeatherapiService.name);
-    @Cron('* * * * * *', {name: 'cronTest'})
+    @Cron('5 * * * * *', {name: 'cronTest'})
     weatherapi() {
         console.log('weather in');
         this.logger.log('Task Called');
