ef71b14dbb1e1f9b96c73937d03db8aaf2a6fec8
ev15963 <ev15963@hs.ac.kr>
2022-10-07 20:42:40 +0900

    cron 동적 제어

--- src/modules/weatherapi/weatherapi.controllers.ts
+++ src/modules/weatherapi/weatherapi.controllers.ts
@@ -1,15 +1,33 @@
 import { Controller, Get } from "@nestjs/common";
+import { SchedulerRegistry } from "@nestjs/schedule";
 import { WeatherapiService } from "./weatherapi.service";
 
 @Controller('/weatherapi')
 export class WeatherapiController {
-    constructor(private readonly weatherapiservice: WeatherapiService) {};
+    // constructor(private ){}
+    constructor(private readonly weatherapiservice: WeatherapiService, private scheduler: SchedulerRegistry) {};
 
-    @Get()
+    @Get('/weather')
     weathertest() {
         const result = this.weatherapiservice.weatherapi();
 
         // return result;
+    }
+
+    @Get('/start-sample')
+    start() {
+        const job = this.scheduler.getCronJob('cronexample');
+
+        job.start();
+        console.log('start!!', job.lastDate());
+    }
+
+    @Get('/stop-sample')
+    stop() {
+        const job = this.scheduler.getCronJob('cronSample');
+
+        job.stop();
+        console.log('stoped', job.lastDate());
     }
 
 }
